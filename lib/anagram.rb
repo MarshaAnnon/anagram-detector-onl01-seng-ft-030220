cfe7f4d08d82126a1fde7f8736b9071ec83c5309
Marsha Annon <marshaannon@gmail.com>
2020-03-15 16:24:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,14 +12,14 @@ class Anagram
 
     def self.match(array)
       array.map do | words |
-        @word.split("").sort == words.split("").sort
+        (@word.split("")).sort == (words.split("")).sort
       end
     end
 
 
 
 
-    
+
 #To determine if they are anagrams, try determining if they are composed of the same letters.
 #Remember that you can split a word into an array of letters using some_word.split(""). You
 #can compare two arrays using the ==
