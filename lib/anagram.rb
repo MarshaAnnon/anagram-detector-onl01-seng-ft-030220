75d2211ece3cc4d603e96a9f697ff3728e2e5014
Marsha Annon <marshaannon@gmail.com>
2020-03-15 16:23:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -19,7 +19,7 @@ class Anagram
 
 
 
-    end
+    
 #To determine if they are anagrams, try determining if they are composed of the same letters.
 #Remember that you can split a word into an array of letters using some_word.split(""). You
 #can compare two arrays using the ==
