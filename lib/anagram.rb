88c36e2df43b28ff98726337d54191eecfae3141
Marsha Annon <marshaannon@gmail.com>
2020-03-15 16:20:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,21 @@
 require "pry"
 class Anagram
 
-  @@all = []
-  attr_accessor :word
+@@match = []
+  
+  attr_accessor :word 
+  
 
     def initialize(word)
       @word = word
-      @@all << self
     end
 
     def self.match
-      @@all << match
+      
+        
+      
     end
-
+#To determine if they are anagrams, try determining if they are composed of the same letters. 
+#Remember that you can split a word into an array of letters using some_word.split(""). You 
+#can compare two arrays using the ==
 end
